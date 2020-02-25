e3856b241afb782faea8fa34bc1534684b24422b
Woo Hyung Choi <whchoi98@gmail.com>
2020-02-25 11:23:55 +0900

    aws ec2 cli add

--- aws_ec2.sh
+++ aws_ec2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # command aws_ec2.sh
-aws ec2 describe-instances --query 'Reservations[].Instances[].[Tags[?Key==`Name`] | [0].Value, Placement.AvailabilityZone,InstanceId, InstanceType, ImageId,State.Name, PrivateIpAddress, PublicIpAddress ]' --output table
+aws ec2 describe-instances --query 'Reservations[].Instances[].[Tags[?Key==`Name`] | [0].Value, Placement.AvailabilityZone,State.Name, PrivateIpAddress, PublicIpAddress ]' --output table
