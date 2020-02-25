5879b589221f8f40dcc955779fdd501c348d3182
Woo Hyung Choi <whchoi98@gmail.com>
2020-02-25 10:14:38 +0900

    add aws shell

---
+++ aws_az.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# command aws_region.sh
+aws ec2 describe-availability-zones --region us-east-1 --output text
---
+++ aws_region.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# command aws_region.sh
+aws ec2 describe-regions --output table
