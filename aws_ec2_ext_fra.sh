b484f493a9e7c908418ecc3a0640ab2e0ca53c0b
WOOHYUNG CHOI <whchoi98@gmail.com>
2022-08-17 19:24:30 +0900

    add shell - nrt,fra,iad

---
+++ aws_ec2_ext_fra.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# command aws_ec2.sh
+aws ec2 describe-instances --query 'Reservations[].Instances[].[Tags[?Key==`Name`] | [0].Value, Placement.AvailabilityZone,InstanceId, InstanceType, ImageId,State.Name, PrivateIpAddress, PublicIpAddress ]' --output table --region eu-central-1
