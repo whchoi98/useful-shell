cab02f0f37a6c55fa5cfa91951de1b66cb376b5a
whchoi98 <whchoi98@gmail.com>
2024-01-26 16:51:44 +0900

    create shell - creat-iam-user-alias.sh, create-cloud9.sh

--- create-iam-user-alias.sh
+++ create-iam-user-alias.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# create user naem and alias
 aws iam create-user --user-name user01
 aws iam create-login-profile --user-name user01 --password 1234Qwer --no-password-reset-required
 aws iam attach-user-policy --user-name user01 --policy-arn arn:aws:iam::aws:policy/AdministratorAccess
