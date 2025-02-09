35644f13e397bf259c6d18b6789ffb49fc447831
whchoi98 <whchoi98@gmail.com>
2025-02-09 05:07:01 +0000

    add create_iam_user.sh

--- create-iam-user-alias.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# create user naem and alias
-aws iam create-user --user-name user01
-aws iam create-login-profile --user-name user01 --password 1234Qwer --no-password-reset-required
-aws iam attach-user-policy --user-name user01 --policy-arn arn:aws:iam::aws:policy/AdministratorAccess
-aws iam create-account-alias --account-alias testalias
-
-
-
